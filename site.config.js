176dc780cde2b75437c2b62c825d8c1866791036
BeiChing <97021845+BeiChing@users.noreply.github.com>
2022-01-03 17:21:51 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9e369b6ebf334935ad7c4f388463be44',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'BEI',
   domain: 'transitivebullsh.it',
   author: 'Travis Fischer',
 
